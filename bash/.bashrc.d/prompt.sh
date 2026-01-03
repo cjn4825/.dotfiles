429f8de9788d43f0112a3c5521e9b6357b77abcf
cjn4825 <Christian4825@gmail.com>
2026-01-02 21:37:13 -0600

    init push of dot files

---
+++ bash/.bashrc.d/prompt.sh
@@ -0,0 +1,13 @@
+# ~/.bashrc.d/prompt.sh
+
+# Git branch and uncommitted status
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+GREEN='\[\033[01;32m\]'
+BLUE='\[\033[01;34m\]'
+ORANGE='\[\033[0;33m\]'
+RESET='\[\033[00m\]'
+
+PS1="${debian_chroot:+($debian_chroot)}${GREEN}\u@\$HOSTNAME${RESET}:${BLUE}\w${ORANGE} \$(parse_git_branch)${RESET}\$ "
