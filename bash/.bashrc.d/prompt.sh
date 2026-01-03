f36697454dfa1a425067e2f903e5af715b69adb2
cjn4825 <Christian4825@gmail.com>
2026-01-03 00:38:49 -0600

    rm test data and added back hostname change depending on environment(just changes on prompt line thing so i'll probably just specify the hostname in running it in the future but for now this is ok)

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -5,9 +5,18 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
+HOST=''
+
+# to see if host is a container or not
+if [ -f /.dockerenv ]; then
+	HOST="docker"
+else
+	HOST="\h"
+fi
+
 GREEN='\[\033[01;32m\]'
 BLUE='\[\033[01;34m\]'
 ORANGE='\[\033[0;33m\]'
 RESET='\[\033[00m\]'
 
-PS1="${debian_chroot:+($debian_chroot)}${GREEN}\u@\$HOSTNAME${RESET}:${BLUE}\w${ORANGE} \$(parse_git_branch)${RESET}\$ "
+PS1="${debian_chroot:+($debian_chroot)}${GREEN}\u@\$HOST${RESET}:${BLUE}\w${ORANGE} \$(parse_git_branch)${RESET}\$ "
