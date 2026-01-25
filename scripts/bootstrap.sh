83c533122c42945437b22038cb73855ce6284590
cjn4825 <Christian4825@gmail.com>
2026-01-25 15:57:09 -0600

    test addition for creating .bashrc.d folder and sourcing automatically...also imporoved linking to use a function instead

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,56 @@
 #!/usr/bin/env bash
 
+set -e
+
 DOTFILES="$HOME/.dotfiles"
 
-# Links files downloaded from github to user environment
-ln -sf "$DOTFILES/bash/.bashrc.d/prompt.sh" "$HOME/.bashrc.d/prompt.sh"
-ln -sf "$DOTFILES/bash/.bashrc.d/tmux_colors.sh" "$HOME/.bashrc.d/tmux_colors.sh"
-ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
-ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+# creates .config if not already
+if [ ! -d $HOME/.config ]; then
+    echo "Creating .config dir..."
+    mkdir -p $HOME/.config
+fi
 
-echo "dotfiles linked to host"
+# creates .bashrc.d if not already
+if [ ! -d $HOME/.bashrc.d ]; then
+    echo "Creating .bashrc.d dir..."
+    mkdir -p $HOME/.bashrc.d
+fi
+
+# creates .local/bin if not already
+if [ ! -d $HOME/.local/bin ]; then
+    echo "Creating .local/bin dir..."
+    mkdir -p $HOME/.local/bin
+fi
+
+echo "Appending source spript to .bashrc..."
+
+# add .bashrc.d to be sourced by .bashrc
+read -r -d '' SCRIPT << 'EOF'
+
+# --- start of dotfiles config link ---
+for file in "$HOME/.bashrc.d/*"; do
+    [ -r "$file" ] && . "$file"
+done
+unset file
+# --- end of dotfile config link ---
+
+EOF
+
+echo "Linking dotfiles to host..."
+
+# append the script to .bashrc
+echo "$SCRIPT" >> "$HOME/.bashrc"
+
+slink() {
+    local dotLocation="$DOTFILES/$1"
+    local confLocation="$HOME/$2"
+
+    ln -sf "$dotLocation" "$confLocation"
+}
+
+# Links files downloaded from github to user environment config locations
+slink "bash/.bashrc.d" ".bashrc.d"
+slink "nvim" ".config/nvim"
+slink "tmux/.tmux.conf" ".tmux.conf"
+
+echo "Dotfiles linked to host!"
